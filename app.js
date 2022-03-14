a3fdf790e4996c9717c3632220d6856b826b1685
Lu-Thao <thaol31@gmatc.matc.edu>
2022-03-14 17:04:31 -0500

    init commit

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let winningCombination = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7],[1,5,9]]
 
-const o = [[]];
-const x = [[]];
+let o = [];
+let x = [];
 let circleTurn;
 var xMark = "X"
 var oMark = "O"
 var winner;
 var message = document.querySelector('.message');
+
+function convert2DArray(array,part) {
+    let newArr = [];
+
+    for(let i = 0; i< array.length; i+=part) {
+        newArr.push(array.slice(i,i+part));
+    }
+
+    return newArr;
+}
+
 
 function turn(){
     return circleTurn != circleTurn;
@@ -27,19 +38,20 @@ return winner;
 function placement(e){
     const box = e.target;
     const id = parseInt(box.id);
-
+    
     if(currentTurn){
         box.textContent = currentTurn;
         currentTurn = currentTurn === oMark ? xMark :oMark;
     }
     if(currentTurn === oMark){
         x.push(id);
+        
     }
     if(currentTurn === xMark){
         o.push(id);
     }
-
     message.textContent = checkWinner();
+    console.log(x);
 }
 
 //compare array
