ad0bf10be769533634e12770741df4fc8238e494
Lu-Thao <thaol31@gmatc.matc.edu>
2022-03-14 15:58:20 -0500

    init commit

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 let winningCombination = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7],[1,5,9]]
 
-let o = [1,3,9];
-const x = [1, 2, 3];
+const o = [[]];
+const x = [[]];
 let circleTurn;
 var xMark = "X"
 var oMark = "O"
+var winner;
+var message = document.querySelector('.message');
+
 function turn(){
     return circleTurn != circleTurn;
 }
 
-console.log(turn())
-const currentTurn = circleTurn ? xMark : oMark;
+let currentTurn = circleTurn ? xMark : oMark;
 
 function checkWinner(){
-    var winner;
+    
 if(winningCombination.some(item => item.every((val,index) => val === o[index]))){
     winner = oMark;
 }else if (winningCombination.some(item => item.every((val,index) => val === x[index]))){
-    winner = xMark
+    winner = xMark;
 }
 return winner;
 }
 
-// function placement(e){
-//     const target = e.target
-//    console.log(target)
+function placement(e){
+    const box = e.target;
+    const id = parseInt(box.id);
 
-// }
-// console.log(placement())
-console.log(checkWinner())
+    if(currentTurn){
+        box.textContent = currentTurn;
+        currentTurn = currentTurn === oMark ? xMark :oMark;
+    }
+    if(currentTurn === oMark){
+        x.push(id);
+    }
+    if(currentTurn === xMark){
+        o.push(id);
+    }
 
+    message.textContent = checkWinner();
+}
 
 //compare array
 // console.log(winningCombination.some(item => item.every((val,index) => val === x[index])));
@@ -37,10 +48,6 @@ console.log(checkWinner())
 
  const button = document.querySelectorAll("button");
  button.forEach((btn)=>{
-     btn.addEventListener("click", function () {
-      
-        btn.textContent = currentTurn;
-         circleTurn = true;
-     });
+     btn.addEventListener("click", placement,{once:true});
      
  })
